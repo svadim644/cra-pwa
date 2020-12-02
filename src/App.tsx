e396cfdc9a3c8e9422d99e4b603028a09fb54d4c
Vadim Suslenkov <v.suslenkov@globogames.ru>
2020-12-02 23:21:17 +0200

    test 3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> ver 23:16
+          Edit <code>src/App.tsx</code> ver 23:21
         </p>
         <a
           className="App-link"
