e13c6dd2240e4180ac9c5ae200923e38f028666b
Vadim Suslenkov <v.suslenkov@globogames.ru>
2020-12-02 23:12:07 +0200

    test 1

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,16 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+    // onSuccess: (registration) => {
+    //     console.log("onSuccess registration", registration);
+    //     window.location.href = "http://localhost:5001/";
+    // },
+    onUpdate: (registration) => {
+        console.log("onUpdate registration", registration);
+        window.location.href = "http://cxpstudio.com/";
+    }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
